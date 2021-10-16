dec32d678fd663781a197596ed68c707c583125b
Olumyco <olumyco4xt@gmail.com>
2021-10-16 21:39:24 +0100

    code modified for posting

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,28 +1,46 @@
+const data = [
+    {
+        id: 1,
+        fullName: 'Jack Kelvin',
+        email: 'jack@gmail.com'
+    },
+    {
+        id: 2,
+        fullName: 'Mark Cook',
+        email: 'mark@gmail.com'
+    },
+    {
+        id: 3,
+        fullName: 'David Calvin',
+        email: 'david@gmail.com'
+    }
+];
+
 exports.getUsers = async (req, res, next) => {
-    try {
-        const data = [
-            {
-                id: 1,
-                fullName: 'Jack Kelvin',
-                email: 'jack@gmail.com'
-            },
-            {
-                id: 2,
-                fullName: 'Mark Cook',
-                email: 'mark@gmail.com'
-            },
-            {
-                id: 3,
-                fullName: 'David Calvin',
-                email: 'david@gmail.com'
-            }
-        ];
-        
+    try { 
         res.json({
             data: data,
             code: 1007,
             success: true,
             message: ''
+        });
+    } catch(err) {
+        res.json({
+            code: 1006,
+            error: true,
+            message: err.message
+        });
+    }
+};
+
+exports.postUser = async (req, res, next) => {
+    try {
+        await data.push(req.body);
+        res.json({
+            data: req.body,
+            code: 1007,
+            success: true,
+            message: 'User details saved successfully'
         });
     } catch(err) {
         res.json({
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 
 router.get('/users', usersController.getUsers);
+router.post('/users', usersController.postUser);
 
 module.exports = router;
